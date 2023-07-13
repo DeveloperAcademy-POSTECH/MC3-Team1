ca14c1615b6ea7e8154bf06668a8c18fadfc16cb
whaeundo <1916388@sookmyung.ac.kr>
2023-07-13 12:17:06 +0900

    [Setting] Init project

---
+++ BrawLab/BrawLab/BrawLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrawLabApp.swift
+//  BrawLab
+//
+//  Created by doeun kim on 2023/07/13.
+//
+
+import SwiftUI
+
+@main
+struct BrawLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
