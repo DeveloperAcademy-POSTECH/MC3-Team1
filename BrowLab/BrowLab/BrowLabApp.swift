7431d59853d7d6ae4bb8bae1246b68f277723f58
whaeundo <1916388@sookmyung.ac.kr>
2023-07-13 12:22:36 +0900

    [Setting] Init project

---
+++ BrowLab/BrowLab/BrowLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrowLabApp.swift
+//  BrowLab
+//
+//  Created by doeun kim on 2023/07/13.
+//
+
+import SwiftUI
+
+@main
+struct BrowLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
