6648949d258f33d4e0e194cdeecf0faf1d5229d8
whaeundo <1916388@sookmyung.ac.kr>
2023-07-27 14:55:25 +0900

    [Feat/#12] Implement hairSemanticSegmentationMatteImage function

--- BrowLab/BrowLab/ViewModel/AVCaptureVM.swift
+++ BrowLab/BrowLab/ViewModel/AVCaptureVM.swift
@@ -1,20 +1,41 @@
-//
+/
 //  AVCaptureVM.swift
 //  BrowLab
 //
-//  Created by doeun kim on 2023/07/27.
+//  Created by doeun kim on 2023/07/22.
 //
+import AVFoundation
+import Foundation
+import UIKit
 
-import SwiftUI
+extension AVCapturePhoto {
+    func hairSemanticSegmentationMatteImage() -> CIImage? {
+        // 머리카락이 있는 부분을 가져옴
+        print("photo: \(self)")
+        if var matte = self.semanticSegmentationMatte(for: .hair) {
+            if let orientation = self.metadata[String(kCGImagePropertyOrientation)] as? UInt32, let exifOrientation = CGImagePropertyOrientation(rawValue: orientation) {
+                matte = matte.applyingExifOrientation(exifOrientation)
+            }
 
-struct AVCaptureVM: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            return CIImage(cvPixelBuffer: matte.mattingImage)
+        }
+        else{
+            print("error - can't make segmentationMatte: \(self.semanticSegmentationMatte(for: .hair))")
+
+        }
+        return nil
     }
-}
 
-struct AVCaptureVM_Previews: PreviewProvider {
-    static var previews: some View {
-        AVCaptureVM()
+    func getPreviewPixelBufferImage() -> CIImage? {
+        if let pixelBuffer = self.previewPixelBuffer {
+            if let orientation = self.metadata[String(kCGImagePropertyOrientation)] as? UInt32, let exifOrientation = CGImagePropertyOrientation(rawValue: orientation) {
+                let ciImage = CIImage(cvPixelBuffer: pixelBuffer).oriented(forExifOrientation: Int32(exifOrientation.rawValue))
+                return ciImage
+            } else {
+                let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
+                return ciImage
+            }
+        }
+        return nil
     }
 }
