6b7108a118780af0f64a0dcab0fd1159fee059ae
whaeundo <1916388@sookmyung.ac.kr>
2023-07-27 14:45:24 +0900

    [Feat/#12] Setting AVCapturePhoto Session

---
+++ BrowLab/BrowLab/ViewModel/AVCaptureVM.swift
@@ -0,0 +1,20 @@
+//
+//  AVCaptureVM.swift
+//  BrowLab
+//
+//  Created by doeun kim on 2023/07/27.
+//
+
+import SwiftUI
+
+struct AVCaptureVM: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AVCaptureVM_Previews: PreviewProvider {
+    static var previews: some View {
+        AVCaptureVM()
+    }
+}
